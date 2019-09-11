eea5dbeb073885ea8ef81d6d2d0f24da765ab2d7
Gui-Lima <ggfl@cin.ufpe.br>
2019-09-11 15:34:04 -0300

    Recycler view and adapter

---
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/Adapters/rcAdapter.kt
@@ -0,0 +1,31 @@
+package br.ufpe.cin.android.podcast.Adapters
+
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.ufpe.cin.android.podcast.ItemFeed
+import br.ufpe.cin.android.podcast.R
+
+class rcAdapter(private val items: ArrayList<ItemFeed>, private val context : Context) : RecyclerView.Adapter<rcAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.itemlista, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -1,12 +1,24 @@
 package br.ufpe.cin.android.podcast
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.FileReader
+import java.io.IOException
+import androidx.recyclerview.widget.LinearLayoutManager
+
+
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        feed_view.layoutManager = LinearLayoutManager(this)
+
+   }
+
 }
